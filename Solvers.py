ff6d55eb1bd26f5b29774dd7ca20c8607b4ad867
brnasantos <45550505+brnasantos@users.noreply.github.com>
2019-02-09 12:32:18 -0200

    Update Solvers.py

--- Solvers.py
+++ Solvers.py
@@ -1,49 +1,55 @@
-from numpy import asarray, prod, diag, dot
-import numpy as *
-import LUdecomp as lu
-import gaussElimin as gauss
-import choleski as ch
-import LUdecomp3 as lu3
-import LUdecomp5 as lu5
-import gaussPivot as gpivot
-import LUpivot
+## module LUpivot
+'''a,seq = LUdecomp(a,tol=1.0e-9).
+LU decomposition of matrix [a] using scaled row pivoting.
+The returned matrix [a] = [L\U] contains [U] in the upper
+triangle and the nondiagonal terms of [L] in the lower triangle.
+Note that [L][U] is a row-wise permutation of the original [a];
+the permutations are recorded in the vector {seq}.
+x = LUsolve(a,b,seq).
+Solves [L][U]{x} = {b}, where the matrix [a] = [L\U] and the 
+permutation vector {seq} are returned from LUdecomp.'''
+    
+def LUdecomp(a,tol=1.0e-9):
+    n = len(a)
+    seq = np.array(range(n))
+    
+  # Set up scale factors
+    s = np.zeros(n)
+    for i in range(n):
+        s[i] = max(abs(a[i,:]))        
+    
+    for k in range(0,n-1):
+        
+      # Row interchange, if needed
+        p = np.argmax(abs(a[k:n,k])/s[k:n]) + k
+        if abs(a[p,k]) <  tol: 
+            print ('matrix is singular')
+            sys.exit()
+        if p != k:
+            swap.swapRows(s,k,p)
+            swap.swapRows(a,k,p)
+            swap.swapRows(seq,k,p)
+            
+      # Elimination            
+        for i in range(k+1,n):
+            if a[i,k] != 0.0:
+                lam = a[i,k]/a[k,k]
+                a[i,k+1:n] = a[i,k+1:n] - lam*a[k,k+1:n]
+                a[i,k] = lam
+    return a,seq
 
-class solvers:
-    def getA(self,L,U):
-        A = dot(L,U)
-        return A
-
-    def get_determinant_from_L_and_U(self,L,U):
-        A = solvers.getA(self,L,U)
-        determinant = prod(diag(A))
-        return determinant
-
-    def get_determinant_from_A(self,A):
-        determinant = prod(diag(A))
-        return determinant
-
-    def byLUdecomp(self,A,B):
-        L = lu.LUdecomp(A)
-        return lu.LUsolve(L,B)
-
-    def byGaussElimin(self,A,B):
-        return gauss.gaussElimin(A,B)
-
-    def byCholeski(self,A,B):
-        L = ch.choleski(A)
-        return ch.choleskiSol(L,B)
-
-    def byLUdecomp3(self,c,d,e,b):
-        c,d,e= lu3.LUdecomp3(c,d,e)
-        return lu3.LUsolve3(c,d,e,b)
-
-    def byLUdecomp5(self,d,e,f,b):
-        d,e,f = lu5.LUdecomp5(d,e,f,b)
-        return lu5.LUsolve5(d,e,f,b)
-
-    def gaussPivot(self,a,b):
-        return gpivot.gaussPivot(a,b)
-
-    def LUpivot(self,a,b):
-        a,seq = LUpivot.LUdecomp(a)
-        return LUpivot.LUsolve(a,b,seq)
+def LUsolve(a,b,seq):
+    n = len(a)
+    
+  # Rearrange constant vector; store it in [x]
+    x = b.copy()
+    for i in range(n):
+        x[i] = b[seq[i]]
+        
+  # Solution
+    for k in range(1,n):
+        x[k] = x[k] - np.dot(a[k,0:k],x[0:k])
+    x[n-1] = x[n-1]/a[n-1,n-1]    
+    for k in range(n-2,-1,-1):
+       x[k] = (x[k] - np.dot(a[k,k+1:n],x[k+1:n]))/a[k,k]
+    return x
